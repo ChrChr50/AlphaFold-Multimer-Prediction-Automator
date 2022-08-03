b1d27fa9824570cc4b99b218167a61fc218dc5fb
Chris Choilee <91154952+ChrChr50@users.noreply.github.com>
2022-08-03 04:04:21 -0600

    Update ChiXAutomate.py

--- ChiXAutomate.py
+++ ChiXAutomate.py
@@ -21,7 +21,57 @@ CLIENT_SECRET = ''
 Box_VideoFolder_ID = ''
 Box_ModelFolder_ID = ''
 VideoFile_Path = ''
-ModelFolder_Path = ''
+
+# Upload model folder and movie to Box
+# The authenticate function definition was acquired from https://github.com/box/box-python-sdk/blob/main/demo/auth.py
+def authenticate(oauth_class = OAuth2):
+    class StoppableWSGIServer(bottle.ServerAdapter):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self._server = None
+
+        def run(self, app):
+            server_cls = self.options.get('server_class', WSGIServer)
+            handler_cls = self.options.get('handler_class', WSGIRequestHandler)
+            self._server = make_server(self.host, self.port, app, server_cls, handler_cls)
+            self._server.serve_forever()
+
+        def stop(self):
+            self._server.shutdown()
+
+    auth_code = {}
+    auth_code_is_available = Event()
+
+    local_oauth_redirect = bottle.Bottle()
+
+    @local_oauth_redirect.get('/')
+    def get_token():
+        auth_code['auth_code'] = bottle.request.query.code
+        auth_code['state'] = bottle.request.query.state
+        auth_code_is_available.set()
+
+    local_server = StoppableWSGIServer(host = 'localhost', port = 8080)
+    server_thread = Thread(target = lambda: local_oauth_redirect.run(server = local_server))
+    server_thread.start()
+
+    oauth = oauth_class(
+        client_id = CLIENT_ID,
+        client_secret = CLIENT_SECRET,
+    )
+    auth_url, csrf_token = oauth.get_authorization_url('http://0.0.0.0')
+    webbrowser.open(auth_url)
+
+    auth_code_is_available.wait()
+    local_server.stop()
+    assert auth_code['state'] == csrf_token
+    access_token, refresh_token = oauth.authenticate(auth_code['auth_code'])
+
+    print('access_token: ' + access_token)
+    print('refresh_token: ' + refresh_token)
+
+    return oauth, access_token, refresh_token
+
+client = Client(authenticate()[0])
 
 # Open spreadsheet and worksheets
 gs = gspread.service_account()
@@ -75,6 +125,8 @@ def iterate(num):
     if seqacc3[num] != 0:
         global third
         third = acc3[num]
+    global file_num
+    file_num = str(num + 1)
 
 class DirHandler(FileSystemEventHandler):
     accum = 0
@@ -89,15 +141,18 @@ class DirHandler(FileSystemEventHandler):
         run(session, 'movie record')
         run(session, 'turn y 2 180')
         run(session, 'wait 180')
-        run(session, 'movie encode')
+        run(session, 'movie encode' + VideoFile_Path)
+        new_video = client.folder(Box_VideoFolder_ID).upload(VideoFile_Path + 'movie' + file_num + '.mp4')
         if third:
             newfile = first + ' - ' + second + ' - ' + third + ' ' + str(date.today())
         else:
             newfile = first + ' - ' + second + ' ' + str(date.today())
+        global newdir
         newdir = os.path.join(AlphaFold_Directory, newfile)
         os.rename(event.src_path, newdir)
 
     def on_moved(self, event: DirMovedEvent):
+        new_folder = client.folder(Box_ModelFolder_ID).upload(newdir)
         if DirHandler.accum < len(seqacc) - 1:
             DirHandler.accum += 1
             iterate(DirHandler.accum)
@@ -109,57 +164,3 @@ actor = DirHandler()
 scan.schedule(actor, path = AlphaFold_Directory)
 scan.start()
 iterate(0)
-
-# Upload model folder and movie to Box
-# The authenticate function definition was acquired from https://github.com/box/box-python-sdk/blob/main/demo/auth.py
-def authenticate(oauth_class = OAuth2):
-    class StoppableWSGIServer(bottle.ServerAdapter):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self._server = None
-
-        def run(self, app):
-            server_cls = self.options.get('server_class', WSGIServer)
-            handler_cls = self.options.get('handler_class', WSGIRequestHandler)
-            self._server = make_server(self.host, self.port, app, server_cls, handler_cls)
-            self._server.serve_forever()
-
-        def stop(self):
-            self._server.shutdown()
-
-    auth_code = {}
-    auth_code_is_available = Event()
-
-    local_oauth_redirect = bottle.Bottle()
-
-    @local_oauth_redirect.get('/')
-    def get_token():
-        auth_code['auth_code'] = bottle.request.query.code
-        auth_code['state'] = bottle.request.query.state
-        auth_code_is_available.set()
-
-    local_server = StoppableWSGIServer(host = 'localhost', port = 8080)
-    server_thread = Thread(target = lambda: local_oauth_redirect.run(server = local_server))
-    server_thread.start()
-
-    oauth = oauth_class(
-        client_id = CLIENT_ID,
-        client_secret = CLIENT_SECRET,
-    )
-    auth_url, csrf_token = oauth.get_authorization_url('http://0.0.0.0')
-    webbrowser.open(auth_url)
-
-    auth_code_is_available.wait()
-    local_server.stop()
-    assert auth_code['state'] == csrf_token
-    access_token, refresh_token = oauth.authenticate(auth_code['auth_code'])
-
-    print('access_token: ' + access_token)
-    print('refresh_token: ' + refresh_token)
-
-    return oauth, access_token, refresh_token
-
-client = Client(authenticate()[0])
-
-new_video = client.folder(Box_VideoFolder_ID).upload(VideoFile_Path)
-new_folder = client.folder(Box_ModelFolder_ID).upload(ModelFolder_Path)
